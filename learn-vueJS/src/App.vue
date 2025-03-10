dec84d25aec7e94f3e21e88668842de5efb76531
anhle1203 <125301207+anhle1203@users.noreply.github.com>
2025-03-09 19:56:15 -0500

    Introduction to Vue Slot

--- learn-vueJS/src/App.vue
+++ learn-vueJS/src/App.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        Hello, Vue!
-    </div>
+  <div class="alert">
+    ERROR:
+    <slot>Oops! Something went wrong.</slot>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
+<style scoped>
+.alert {
+  background-color: #f0c3c0;
+  padding: 15px;
+  margin: 10px 0;
+  border: 1px solid #e7a9a7;
+  border-radius: 4px;
+  font-family: Arial, sans-serif;
+  font-size: 14px;
+}
 </style>
